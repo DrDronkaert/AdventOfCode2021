e8b3b835d21293e779ab42f1ccf90a5ac60cb1f0
manufourneau <fourneaumanu@gmail.com>
2021-12-03 09:39:52 +0100

    rename

--- Speeltuin/Days.cs
+++ Speeltuin/Days.cs
@@ -6,9 +6,9 @@ using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
-namespace Speeltuin
+namespace AdventOfCode
 {
-    class Kata
+    class Days
     {
         public static void AdventOfCode11()
         {
--- Speeltuin/Program.cs
+++ Speeltuin/Program.cs
@@ -1,23 +1,23 @@
 ﻿using System;
 
-namespace Speeltuin
+namespace AdventOfCode
 {
     class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("11:");
-            Kata.AdventOfCode11();
+            Days.AdventOfCode11();
             Console.WriteLine("12:");
-            Kata.AdventOfCode12();
+            Days.AdventOfCode12();
             Console.WriteLine("21:");
-            Kata.AdventOfCode21();
+            Days.AdventOfCode21();
             Console.WriteLine("22:");
-            Kata.AdventOfCode22();
+            Days.AdventOfCode22();
             Console.WriteLine("31:");
-            Kata.AdventOfCode31();
+            Days.AdventOfCode31();
             Console.WriteLine("32:");
-            Kata.AdventOfCode32();
+            Days.AdventOfCode32();
         }
     }
 }
